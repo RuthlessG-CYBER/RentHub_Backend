d6c5a445356214ec6c916b74270f88ec6b913798
RuthlessG-CYBER <pandasoumya604@gmail.com>
2025-11-24 11:26:58 +0530

    RentHub_Backend

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const MONGODB_URL = process.env.MONGODB_URL;
 app.use(
   cors({
     origin: [
-      // "http://localhost:5173",
+      "http://localhost:5173",
       "https://rent-hub-frontend-five.vercel.app/"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
